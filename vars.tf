f45d3d9fdc62ffffc6636c1294581f8c5a6b9f0e
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-10-09 13:24:05 +0530

    a

--- vars.tf
+++ vars.tf
@@ -1,5 +1,7 @@
 variable "parameters" {
-  type = map(string)
+  type = map(tuple([ 
+    
+   ]))
   default = [
     {name = "prod.rds.master_username", type ="String", value="admin"},
     {name = "prod.rds.master_password", type = "SecureString", value="Harsha123"}
