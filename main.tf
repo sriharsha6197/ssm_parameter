67869bd1eaa538e50b388895f7b1375e53833fce
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-10-09 13:48:51 +0530

    a

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_ssm_parameter" "foo" {
-  for_each = length(var.parameters)
+  count = length(var.parameters)
   name = var.parameters[count.index].name
   type = var.parameters[count.index].type
   value = var.parameters[count.index].value
